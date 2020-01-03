69b89cb8620b60c7c6d923100b4e8cdf5f89beab
jungminlee <dlwjdals6424@naver.com>
2020-01-03 15:15:43 +0900

    les

--- Nouveau document texte.js
+++ Nouveau document texte.js
@@ -8,6 +8,8 @@ import {
   StatusBar,
   Navigator,
   Button,
+  TextInput,
+  FlatList,
 } from 'react-native';
 import { createAppContainer, NavigationScreenProps } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -18,17 +20,83 @@ import Test2 from './src/component/test2';
 import Test3 from './src/component/test3';
 
 class Apps extends Component {
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: false, text: ''};
+    this.arrayholder = [];
+    this.isLoadings = 'this is working';
+  }
+
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function(item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.name? item.name.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      text: text,
+    });
+  }
+componentDidMount(){
+  return fetch('https://jsonplaceholder.typicode.com/users')
+  //return fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      }, function(){
+        this.arrayholder = responseJson;
+      });
+
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+}
+ //<Text >{item.title} 'is running ther business' {item.releaseYear} </Text>
+  _renderItem = ({item}) => (
+
+  //  <TouchableHighlight onPress={() => {this.onPress(item).bind(this)}}>
+    <View style= {{flex: 1, padding: 20}} >
+      <Text >{item.name} 'is running ther business' {item.company.name} </Text>
+
+    </View>
+  //  </TouchableHighlight>
+);
+
   render() {
     console.warn('hiiiiddd');
     return (
       <View style={styles.container}>
-        <View style={styles.case1} />
+        <View style={styles.case1} >
+          <TextInput
+         style={styles.textInputStyle}
+         onChangeText={text => this.SearchFilterFunction(text)}
+         value={this.state.text}
+         underlineColorAndroid="transparent"
+         placeholder="Search Here"
+       />
+      </View>
         <View style={styles.case2}>
         <Separator />
-          <Text> 'goood Jung!!'</Text>
+
+          <FlatList
+            data={this.state.dataSource}
+            renderItem={this._renderItem}
+            style={{ marginTop: 10 }}
+            keyExtractor={item => item.id.toString()}
+          />
+
           <Separator />
         </View>
-        <Separator />
+
         <View style={styles.case3}>
           <View style={styles.case4} >
           <Icon  name="ios-search" size={70} onPress={() => this.props.navigation.navigate('Test1')}
@@ -59,8 +127,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   case1: {
-    flex: 1,
-    backgroundColor: 'blue',
+    flex: 0.5,
+    //backgroundColor: 'blue',
   },
   case2: {
     flex: 3,
@@ -76,11 +144,11 @@ const styles = StyleSheet.create({
   },
   case4: {
     flex: 1,
-    backgroundColor: 'powderblue',
+    //backgroundColor: 'powderblue',
   },
   case5: {
     flex: 1,
-    backgroundColor: 'skyblue',
+    //backgroundColor: 'skyblue',
     //marginBottom: 10,
   },
   case6: {
@@ -93,6 +161,13 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     borderBottomColor: '#737373',
     borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+    textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
   },
 });
 
