9fab0d7d6fe593ad7c2661dcdcb9afa5638e61a8
jungminlee <dlwjdals6424@naver.com>
2020-01-28 00:06:45 +0900

    session m

--- component/Session.js
+++ component/Session.js
@@ -28,6 +28,26 @@ AsyncStorage.setItem('data', data);
 
 }
 
+_saveDatas = () => {
+    const obj = {
+
+'name':'hide',
+
+'phone':'010-1234-5678',
+
+'dept':'computer'
+
+}
+
+AsyncStorage.setItem('obj', JSON.stringify(obj));
+
+}
+
+
+_removeData = () => {
+  AsyncStorage.removeItem('data');
+}
+
 _touchevent = () =>{
   console.log('This is event');
 }
@@ -39,12 +59,31 @@ _retrieveData = async () => {
       // We have data!!
       console.log(value);
     }
+    if(value == null){
+      console.log('this is expired');
+    }
   } catch (error) {
     // Error retrieving data
     console.log(error);
   }
 };
 
+_retrieveDatas = async () => {
+  try {
+    const value = await AsyncStorage.getItem('obj',(err, result,store) => {
+
+    });
+    if (value !== null) {
+      // We have data!!
+      let json = JSON.parse(value);
+      console.log(json['name']);
+    }
+
+  } catch (error) {
+    // Error retrieving data
+    console.log(error);
+  }
+};
 
   render() {
     console.log('this is navigation'+this.props.navigation);
@@ -64,6 +103,24 @@ _retrieveData = async () => {
           //onPress={() => this.props.navigation.navigate('Test2')}
           title="Show"
         />
+        <Button
+         color="#f194ff"
+         onPress={this._removeData}
+         //onPress={() => this.props.navigation.navigate('Test2')}
+         title="remove"
+       />
+       <Button
+        color="#f194ff"
+        onPress={this._saveDatas}
+        //onPress={() => this.props.navigation.navigate('Test2')}
+        title="Json"
+      />
+      <Button
+       color="#f194ff"
+       onPress={this._retrieveDatas}
+       //onPress={() => this.props.navigation.navigate('Test2')}
+       title="SeeJson"
+     />
       </View>
       </SafeAreaView>
 
